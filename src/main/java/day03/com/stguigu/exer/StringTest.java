3dcfdcdf102f3e7f38f7e259de60b7287c58df34
陈陆 <969117645@qq.com>
2022-12-06 21:39:52 +0800

    221206晚上练习

---
+++ src/main/java/day03/com/stguigu/exer/StringTest.java
@@ -0,0 +1,20 @@
+package day03.com.stguigu.exer;
+
+/**
+ * @author shkstart
+ * @create 2022-12-06 21:12
+ */
+public class StringTest {
+    String str = new String("good");
+    char[] ch = { 't', 'e', 's', 't' };
+    public void change(String str, char ch[]) {
+        str = "test ok";
+        ch[0] = 'b';
+    }
+    public static void main(String[] args) {
+        StringTest ex = new StringTest();
+        ex.change(ex.str, ex.ch);
+        System.out.println(ex.str);//good
+        System.out.println(ex.ch);//best
+    }
+}
---
+++ src/main/java/day03/com/stguigu/java/Person.java
@@ -0,0 +1,18 @@
+package day03.com.stguigu.java;
+
+/**
+ * @author shkstart
+ * @create 2022-12-06 20:45
+ */
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+}
