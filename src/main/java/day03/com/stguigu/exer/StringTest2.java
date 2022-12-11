1707caafcf3cba7d9e18cb79daab5455324857a8
陈陆 <969117645@qq.com>
2022-12-11 21:40:48 +0800

    221211晚上练习题 前提：两个字符串中只有一个最大相同子串 将短的那个串进行长度依次递减的子串与较长的串比较 获取subStr在mainStr中出现的次数

--- src/main/java/day03/com/stguigu/exer/StringTest2.java
+++ src/main/java/day03/com/stguigu/exer/StringTest2.java
@@ -83,4 +83,120 @@ public class StringTest2 {
         System.out.println(str1);// abfedcg
 
     }
+    //* 3. 获取一个字符串在另一个字符串中出现的次数。比如：获取“ ab”在 “abkkcadkabkebfkabkskab” 中出现的次数
+    /**
+     * 获取subStr在mainStr中出现的次数
+     *
+     */
+    public int getCount(String mainStr,String subStr){
+        int mainLength=mainStr.length();
+        int sublength=subStr.length();
+        int count=0;
+        int index=0;
+        if (mainLength>=sublength){
+            //方式一：
+//            while ((index=mainStr.indexOf(subStr))!=-1){
+//                count++;
+//                mainStr=mainStr.substring(subStr.length() + index);
+//            }
+            //方式二：改进
+            while ((index=mainStr.indexOf(subStr,index))!=-1){
+                count++;
+                index+=sublength;
+            }
+            return count;
+        }else {
+            return 0;
+        }
+
+    }
+    @Test
+    public void testGetCount(){
+        String mainStr="abkkcadkabkebfkabkskab";
+        String subStr="ab";
+        int count = getCount(mainStr, subStr);
+        System.out.println(count);
+    }
+    //* 4.获取两个字符串中最大相同子串。比如：str1 = "abcwerthelloyuiodef“;str2 = "cvhellobnm"
+    // 提示：将短的那个串进行长度依次递减的子串与较长的串比较。
+    //前提：两个字符串中只有一个最大相同子串
+    public String getMaxSameStringg(String str1,String str2){
+        if (str1!=null&&str2!=null){
+            String maxStr=(str1.length()>=str2.length())?str1:str2;
+            String minStr=(str1.length()<str2.length())?str1:str2;
+            int length=minStr.length();
+            for (int i=0;i<length;i++){
+                for (int x=0,y=length-i;y<=length;x++,y++){
+                    String subStr=minStr.substring(x,y);
+                    if (maxStr.contains(subStr)){
+                        return subStr;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+    // 如果存在多个长度相同的最大相同子串
+    // 此时先返回String[]，后面可以用集合中的ArrayList替换，较方便
+    public String[] getMaxSameSubString1(String str1, String str2) {
+        if (str1 != null && str2 != null) {
+            StringBuffer sBuffer = new StringBuffer();
+            String maxString = (str1.length() > str2.length()) ? str1 : str2;
+            String minString = (str1.length() > str2.length()) ? str2 : str1;
+
+            int len = minString.length();
+            for (int i = 0; i < len; i++) {
+                for (int x = 0, y = len - i; y <= len; x++, y++) {
+                    String subString = minString.substring(x, y);
+                    if (maxString.contains(subString)) {
+                        sBuffer.append(subString + ",");
+                    }
+                }
+                System.out.println(sBuffer);
+                if (sBuffer.length() != 0) {
+                    break;
+                }
+            }
+            String[] split = sBuffer.toString().replaceAll(",$", "").split("\\,");
+            return split;
+        }
+
+        return null;
+    }
+    // 如果存在多个长度相同的最大相同子串：使用ArrayList
+//	public List<String> getMaxSameSubString1(String str1, String str2) {
+//		if (str1 != null && str2 != null) {
+//			List<String> list = new ArrayList<String>();
+//			String maxString = (str1.length() > str2.length()) ? str1 : str2;
+//			String minString = (str1.length() > str2.length()) ? str2 : str1;
+//
+//			int len = minString.length();
+//			for (int i = 0; i < len; i++) {
+//				for (int x = 0, y = len - i; y <= len; x++, y++) {
+//					String subString = minString.substring(x, y);
+//					if (maxString.contains(subString)) {
+//						list.add(subString);
+//					}
+//				}
+//				if (list.size() != 0) {
+//					break;
+//				}
+//			}
+//			return list;
+//		}
+//
+//		return null;
+//	}
+    @Test
+    public void TestGetMaxSameString(){
+        String str1="abcwerthelloyuiodef";
+        String str2="cvhellobnm";
+        String maxSameStringg = getMaxSameStringg(str1, str2);
+        System.out.println(maxSameStringg);
+
+    }
+
+
 }
+
