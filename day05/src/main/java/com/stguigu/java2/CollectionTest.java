d842cb3fbd50d12991fa32f068407c8e1dc62b03
陈陆 <969117645@qq.com>
2022-12-15 21:50:36 +0800

    221215晚上练习：集合

--- day05/src/main/java/com/stguigu/java2/CollectionTest.java
+++ day05/src/main/java/com/stguigu/java2/CollectionTest.java
@@ -1,5 +1,11 @@
 package com.stguigu.java2;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
 /**
  * 一、集合框架的概述
  *
@@ -16,12 +22,49 @@ package com.stguigu.java2;
  *      >数组中提供的方法非常有限，对于添加、删除、插入数据等操作，非常不便，同时效率不高。
  *      >获取数组中实际元素的个数的需求，数组没有现成的属性或方法可用
  *      >数组存储数据的特点：有序、可重复。对无序、不可重复的需求，不能满足。
-  *
  *
  *
+ * 二、集合框架
+ *      |----Collection接口：单列集合，用来存储一个一个的对象
+ *         |----list接口：存储有序的、可重复的数据。  --->“动态“数组
+ *              |----ArrayList、LinkedList、Vector
+ *
+ *         |----set接口：存储无序的、不可重复的数据   ---->高中讲的“集合”
+ *              |----HashSet、LinkedHashSet、TreeSet
+ *
+ *      |----Map接口：双列集合，用来存储一对（key---value）一对的数据  -->高中函数：y=f(x)
+ *              |----HashMap、linkedHashMap、TreeMap、Hashtable、Properties
+ *三、Collection接口中的方法的使用
+
  *
  * @author shkstart
  * @create 2022-12-14 21:49
  */
 public class CollectionTest {
+    @Test
+    public void test1(){
+        Collection coll=new ArrayList();
+
+        //add(Object e):将元素e添加到集合coll中
+        coll.add("AA");
+        coll.add("CC");
+        coll.add(123);//自动装箱
+        coll.add(new Date());
+
+        //size()：获取添加元素的个数
+        System.out.println(coll.size());
+        //addAll():将coll1集合中的元素添加到当前集合中
+        Collection coll1=new ArrayList();
+        coll1.add(456);
+        coll1.add("DD");
+        coll.addAll(coll1);
+        System.out.println(coll.size());
+        System.out.println(coll);
+        //clear()：清空集合元素
+        coll.clear();
+        //isEmpty()：判断当前集合是否为空
+        System.out.println(coll.isEmpty());
+
+
+    }
 }
