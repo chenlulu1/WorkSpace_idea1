b523e7fdc685cd392c829d503bf40207838fa4a9
陈陆 <969117645@qq.com>
2022-12-20 21:56:56 +0800

    221220晚上练习：集合

---
+++ src/main/java/day07/com/stguigu/exer1/CollectionTest.java
@@ -0,0 +1,62 @@
+package day07.com.stguigu.exer1;
+
+import org.junit.Test;
+
+import java.awt.datatransfer.FlavorListener;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * @author shkstart
+ * @create 2022-12-20 20:34
+ */
+public class CollectionTest {
+    @Test
+    public void test1(){
+        Collection list = new ArrayList();
+        list.add(123);
+        list.add(456);
+        list.add(147);
+        list.add(147);
+        list.forEach(System.out::println);
+    }
+    //练习：在List内去除重复数字值，要求尽量简单
+    public static List duplicateList(List list) {
+        HashSet set = new HashSet();
+        set.addAll(list);
+        return new ArrayList(set);
+    }
+    @Test
+
+    public void test2() {
+        List list = new ArrayList();
+        list.add(new Integer(1));
+        list.add(new Integer(2));
+        list.add(new Integer(2));
+        list.add(new Integer(4));
+        list.add(new Integer(4));
+        List list2 = duplicateList(list);
+        for (Object integer : list2) {
+            System.out.println(integer);
+        }
+    }
+    @Test
+    public void test3(){
+        HashSet set = new HashSet();
+        Person p1 = new Person(1001,"AA");
+        Person p2 = new Person(1002,"BB");
+        set.add(p1);
+        set.add(p2);
+        System.out.println(set);
+        p1.name = "CC";
+        set.remove(p1);
+        System.out.println(set);
+        set.add(new Person(1001,"CC"));
+        System.out.println(set);
+        set.add(new Person(1001,"AA"));
+        System.out.println(set);
+
+    }
+}
---
+++ src/main/java/day07/com/stguigu/exer1/Person.java
@@ -0,0 +1,42 @@
+package day07.com.stguigu.exer1;
+
+import java.util.Objects;
+
+/**
+ * @author shkstart
+ * @create 2022-12-20 20:40
+ */
+public class Person {
+    int id;
+    String name;
+
+    public Person() {
+    }
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
